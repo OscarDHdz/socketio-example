614550b94ce3c7d6568999b391092c04b3f55e68
Oscar Hernandez <oscard.hernandez@softtek.com>
2017-01-25 12:39:33 -0600

    Connect Disconnec handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
-const path = require('path');
-const express = require('express');
-
-var app  = express();
-var port = 3000;
+const path     = require('path');
+const http     = require('http');
+const express  = require('express');
+const socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname, '../public');
-console.log(__dirname + '/../public');
-console.log(publicPath);
+console.log('Public path:', publicPath);
+var app    = express();
+var port   = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io     = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.error('Disconnected user');
+  });
+
+});
+
+
+
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
