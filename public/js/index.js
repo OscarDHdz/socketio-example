63113fdb9e95da504b2478a8a93cf4c428f0ca88
Oscar Hernandez <oscard.hernandez@softtek.com>
2017-01-25 15:24:43 -0600

    Add new Message and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Oscar',
+    text: ';) !!!!!'
+  });
+
+
+});
+
+socket.on('newMessage', function ( message ) {
+  console.log('newMessage', message);
+});
+
+
+socket.on('disconnect', function () {
+  console.error('Disconnected from server');
+});
